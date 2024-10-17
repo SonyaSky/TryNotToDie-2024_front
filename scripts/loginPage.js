e22f94051886cf7c33525cc9083e62af9a1bc79c
SonyaSky <sonyasidonya@gmail.com>
2024-10-17 14:21:31 +0700

    registration page
    I made a registration page. You can get there from login page. Scripts and css file are now in folders

---
+++ scripts/loginPage.js
@@ -0,0 +1,5 @@
+const sign_up_button = document.getElementById("sign-up-button");
+
+sign_up_button.addEventListener('click', function() {
+    window.location.href = "registration.html"
+});
---
+++ scripts/registrationPage.js
@@ -0,0 +1,5 @@
+const enter_button = document.getElementById("enter");
+
+enter_button.addEventListener('click', function() {
+    window.location.href = "login.html"
+});
