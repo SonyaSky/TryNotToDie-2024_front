d84fe40cf86e038fb4086d23b037e725dffe830f
Sofya Sidonskaya <sonyasidonya@gmail.com>
2024-10-19 21:17:15 +0700

    token and specialities
    Now you can login. Kind of. It's just sending a post request and getting token. Also you can now see all specialities on registration page. And of course mask for phone number input

--- scripts/loginPage.js
+++ scripts/loginPage.js
@@ -3,3 +3,59 @@ const sign_up_button = document.getElementById("sign-up-button");
 sign_up_button.addEventListener('click', function() {
     window.location.href = "registration.html"
 });
+
+
+
+const sign_in_button = document.getElementById("sign-in-button");
+const input_email = document.getElementById("inputEmail");
+const input_password = document.getElementById("inputPassword");
+const login_form = document.getElementById("login-form");
+
+
+login_form.addEventListener('submit', (e) => {
+    e.preventDefault(); 
+
+    if (!input_email.checkValidity()) {
+        return; 
+    }
+
+    const email = input_email.value;
+    const password = input_password.value;
+
+    if (!email) {
+        alert("Введите email, пожалуйста!");
+        return;
+    } 
+    if (!password) {
+        alert("Введите пароль, пожалуйста!");
+        return;
+    } 
+
+    loginDoctor(email, password).then((data) => {
+        token = data;
+        console.log(token);
+    })
+    .catch((error) => console.error(error));
+
+
+});
+
+
+function loginDoctor(email, password) 
+{
+    const loginDto = {
+        "email": email,
+        "password": password
+    }
+
+    return fetch('https://mis-api.kreosoft.space/api/doctor/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+          },
+        body: JSON.stringify(loginDto),
+      })
+    .then((response) => response.json())
+    .then((data) => data);
+}
+
--- scripts/registrationPage.js
+++ scripts/registrationPage.js
@@ -1,5 +1,39 @@
-const enter_button = document.getElementById("enter");
+//загрузили специальности врачей
+function fetchSpecialties() {
+    return fetch('https://mis-api.kreosoft.space/api/dictionary/speciality?page=1&size=25')
+    .then((data) => {
+        return data.json();
+    }).then((info) => {
+        return info;
+    });
+}
 
-enter_button.addEventListener('click', function() {
-    window.location.href = "login.html"
+fetchSpecialties().then((data) => {
+    populateSelect(JSON.stringify(data));
 });
+
+function populateSelect(s) {
+    
+    const selectElement = document.getElementById('inputSpeciality');
+        
+    selectElement.innerHTML = '';
+    
+    JSON.parse(s).specialties.forEach(option => {
+        const opt = document.createElement('option');
+        opt.textContent = option.name;
+        selectElement.appendChild(opt);
+    });
+}
+    
+
+document.addEventListener('DOMContentLoaded', populateSelect);
+
+
+
+
+
+const sign_in_button = document.getElementById("sign-in-button");
+
+sign_in_button.addEventListener('submit', (e) => {
+    
+})
